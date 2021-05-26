a71c169d441ce197f807a2fb28d429e1c524050d
Adolfo <adolfoprieto37@yahoo.com>
2021-05-26 17:15:35 -0500

    create tile layer

--- Leaflet-step1/static/js/logic.js
+++ Leaflet-step1/static/js/logic.js
@@ -5,3 +5,11 @@ var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day
 var earthquakes = L.layerGroup();
 
 //Create Tile Layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY,
+}).addTo(myMap);
