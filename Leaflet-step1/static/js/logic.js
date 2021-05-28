9d091b9b1af9c43ce82622a1d972ebe59c2a4806
Adolfo <adolfoprieto37@yahoo.com>
2021-05-28 10:37:54 -0500

    push

--- Leaflet-step1/static/js/logic.js
+++ Leaflet-step1/static/js/logic.js
@@ -1,20 +1,6 @@
 //Start Code
-//Create Tile Layer
-function createMap(earthquakeLayer) {
-  var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY,
-  })
 
-  var myMap = L.map("mapid", {
-    center: [37.6872, -97.3301],
-    zoom: 4
-  });
-  tileLayer.addTo(myMap);
+
 //Load in geojson data
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
@@ -44,15 +30,37 @@ function earthquakes(depth) {
     }
   }
 
-// GeoJSON layer
-L.geoJson(depth, {
-  // Maken cricles
-  pointToLayer: function(feature, coordinates) {
-    return L.circleMarker(coordinates);
-  },
-}).addTo(myMap);
+  var earthquakeslayer = L.layerGroup(feature)
 
-};
+  function createFeatures(earthquakeData) {
 
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
 
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+    createMap(earthquakes);
+  }
+}
+
+//Create Tile Layer
+function createMap(earthquakeLayer) {
+  var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY,
+  })
+
+  var myMap = L.map("mapid", {
+    center: [37.6872, -97.3301],
+    zoom: 4
+    layers: [tileLayer, earthquakesLayer]
+  });
+  tileLayer.addTo(myMap);
 }
