62bf5bed61611557320097d527ae3791714b3bea
Adolfo <adolfoprieto37@yahoo.com>
2021-05-28 10:44:26 -0500

    10:44am Friday

--- Leaflet-step1/static/js/logic.js
+++ Leaflet-step1/static/js/logic.js
@@ -1,6 +1,4 @@
 //Start Code
-
-
 //Load in geojson data
 var baseURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
@@ -29,25 +27,9 @@ function earthquakes(depth) {
       color = "light blue";
     }
   }
-
-  var earthquakeslayer = L.layerGroup(feature)
-
-  function createFeatures(earthquakeData) {
-
-    function onEachFeature(feature, layer) {
-      layer.bindPopup("<h3>" + feature.properties.place +
-        "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-    }
-
-    var earthquakes = L.geoJSON(earthquakeData, {
-      onEachFeature: onEachFeature
-    });
-    createMap(earthquakes);
-  }
 }
 
 //Create Tile Layer
-function createMap(earthquakeLayer) {
   var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -59,8 +41,8 @@ function createMap(earthquakeLayer) {
 
   var myMap = L.map("mapid", {
     center: [37.6872, -97.3301],
-    zoom: 4
-    layers: [tileLayer, earthquakesLayer]
+    zoom: 4,
   });
   tileLayer.addTo(myMap);
-}
+  
+  createImageBitmap()
