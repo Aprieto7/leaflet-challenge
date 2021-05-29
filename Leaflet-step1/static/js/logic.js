f33af4c311db466f4aaa6df0eeea23cbbaa5d6ce
Adolfo <adolfoprieto37@yahoo.com>
2021-05-29 15:08:24 -0500

    update

--- Leaflet-step1/static/js/logic.js
+++ Leaflet-step1/static/js/logic.js
@@ -44,7 +44,7 @@ function earthquakes(depth) {
   }
 }
 
-function createMap(earthquakes) {
+function createMap() {
   //Create Tile Layer
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
